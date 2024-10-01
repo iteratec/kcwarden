167a99c83585acbe5bece8649327ad325bb2d584
Max Maass <max.maass@iteratec.com>
2024-10-01 14:32:42 +0200

    Include SAML support

--- tests/auditors/idp/test_oidc_identity_provider_with_signature_verification_disabled.py
+++ tests/auditors/idp/test_oidc_identity_provider_with_signature_verification_disabled.py
@@ -1,8 +1,8 @@
 import pytest
 from unittest.mock import Mock
 
-from kcwarden.auditors.idp.oidc_identity_provider_with_signature_verification_disabled import (
-    OIDCIdentityProviderWithSignatureVerificationDisabled,
+from kcwarden.auditors.idp.identity_provider_with_signature_verification_disabled import (
+    IdentityProviderWithSignatureVerificationDisabled,
 )
 from kcwarden.custom_types import config_keys
 
@@ -10,7 +10,7 @@ from kcwarden.custom_types import config_keys
 class TestOIDCIdentityProviderWithSignatureVerificationDisabled:
     @pytest.fixture
     def auditor(self, database, default_config):
-        auditor_instance = OIDCIdentityProviderWithSignatureVerificationDisabled(database, default_config)
+        auditor_instance = IdentityProviderWithSignatureVerificationDisabled(database, default_config)
         auditor_instance._DB = Mock()
         return auditor_instance
 
@@ -19,7 +19,8 @@ class TestOIDCIdentityProviderWithSignatureVerificationDisabled:
         [
             ("oidc", True),  # OIDC provider should be considered
             ("keycloak-oidc", True),  # Keycloak OIDC provider should be considered
-            ("saml", False),  # SAML provider should not be considered
+            ("saml", True),  # SAML provider should also be considered
+            ("github", False),  # SAML provider should also be considered
         ],
     )
     def test_should_consider_idp(self, auditor, provider_id, expected):
@@ -69,9 +70,13 @@ class TestOIDCIdentityProviderWithSignatureVerificationDisabled:
         idp3.get_provider_id.return_value = "keycloak-oidc"
         idp3.get_config.return_value = {"validateSignature": "false"}
 
-        auditor._DB.get_all_identity_providers.return_value = [idp1, idp2, idp3]
+        idp4 = Mock()
+        idp4.get_provider_id.return_value = "saml"
+        idp4.get_config.return_value = {"validateSignature": "false"}
+
+        auditor._DB.get_all_identity_providers.return_value = [idp1, idp2, idp3, idp4]
         results = list(auditor.audit())
-        assert len(results) == 2  # Expect findings from idp1 and idp3
+        assert len(results) == 3  # Expect findings from idp1, idp3 and idp4
 
     def test_ignore_list_functionality(self, auditor, mock_idp):
         # Setup IDP without force sync mode and with mappers
