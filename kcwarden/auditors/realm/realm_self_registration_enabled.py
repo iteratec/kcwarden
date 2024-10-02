1b4aedfbbbfce93f1e807f821f17b2d583afa3e8
Tim Walter <tim.walter@iteratec.com>
2024-10-02 11:46:08 +0200

    Use the generic parent class for all realm auditors
    Signed-off-by: Tim Walter <tim.walter@iteratec.com>

--- kcwarden/auditors/realm/realm_self_registration_enabled.py
+++ kcwarden/auditors/realm/realm_self_registration_enabled.py
@@ -1,21 +1,20 @@
-from kcwarden.api import Auditor
-from kcwarden.custom_types.result import Severity
+from typing import Generator
+
+from kcwarden.auditors.realm.abstract_realm_auditor import AbstractRealmAuditor
+from kcwarden.custom_types.keycloak_object import Realm
+from kcwarden.custom_types.result import Severity, Result
 
 
-class RealmSelfRegistrationEnabled(Auditor):
+class RealmSelfRegistrationEnabled(AbstractRealmAuditor):
     DEFAULT_SEVERITY = Severity.Info
     SHORT_DESCRIPTION = "Self-Registration enabled"
     LONG_DESCRIPTION = "The realm supports self-registration, which means that anyone can register an account. In some cases, this may not be desired, hence kcwarden is flagging this behavior."
     REFERENCE = ""
 
-    def should_consider_realm(self, realm) -> bool:
-        return self.is_not_ignored(realm)
-
-    def realm_has_self_registration_enabled(self, realm) -> bool:
+    @staticmethod
+    def realm_has_self_registration_enabled(realm) -> bool:
         return realm.is_self_registration_enabled()
 
-    def audit(self):
-        for realm in self._DB.get_all_realms():
-            if self.should_consider_realm(realm):
-                if self.realm_has_self_registration_enabled(realm):
-                    yield self.generate_finding(realm)
+    def audit_realm(self, realm: Realm) -> Generator[Result, None, None]:
+        if self.realm_has_self_registration_enabled(realm):
+            yield self.generate_finding(realm)
