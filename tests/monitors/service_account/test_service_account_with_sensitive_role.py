619c19339c1f7988d4ade09b5feb28622e99aee4
Max Maass <max.maass@iteratec.com>
2024-12-17 10:11:54 +0100

    Add tests for ServiceAccountWithSensitiveRole Monitor

---
+++ tests/monitors/service_account/test_service_account_with_sensitive_role.py
@@ -0,0 +1,175 @@
+import pytest
+from unittest.mock import Mock
+
+from kcwarden.monitors.service_account.service_account_with_sensitive_role import ServiceAccountWithSensitiveRole
+from kcwarden.custom_types import config_keys
+
+
+class TestServiceAccountWithSensitiveRole:
+    @pytest.fixture
+    def monitor(self, database, default_config):
+        monitor_instance = ServiceAccountWithSensitiveRole(database, default_config)
+        monitor_instance._DB = Mock()
+        return monitor_instance
+
+    @pytest.fixture
+    def mock_service_account(self):
+        service_account = Mock()
+        service_account.get_name.return_value = "test-service-account"
+        service_account.get_client_id.return_value = "test-client-id"
+        service_account.get_realm_roles.return_value = ["test-realm-role"]
+        service_account.get_client_roles.return_value = {"test-client": ["test-client-role"]}
+        service_account.get_groups.return_value = ["test-group"]
+        return service_account
+
+    # Unit tests using Mocks
+    def test_should_consider_service_account(self, monitor, mock_service_account):
+        allowed_service_accounts = ["allowed-.*"]
+        assert monitor._should_consider_service_account(mock_service_account, allowed_service_accounts) is True
+
+        mock_service_account.get_name.return_value = "allowed-service-account"
+        assert monitor._should_consider_service_account(mock_service_account, allowed_service_accounts) is False
+
+    @pytest.mark.parametrize(
+        "ignore_disabled_clients, client_enabled, expected_result, is_client_object",
+        [
+            (True, False, True, True),  # Disabled client, config to ignore
+            (True, True, False, True),  # Enabled client, should never be ignored
+            (False, False, False, True),  # Disabled client, config not to ignore
+            (True, False, False, False),  # Non-Client object, should always return False
+        ],
+    )
+    def test_is_ignored_disabled_client(
+        self, monitor, mock_client, ignore_disabled_clients, client_enabled, expected_result, is_client_object
+    ):
+        monitor._CONFIG = {config_keys.IGNORE_DISABLED_CLIENTS: ignore_disabled_clients}
+
+        if is_client_object:
+            mock_client.is_enabled.return_value = client_enabled
+            test_object = mock_client
+        else:
+            test_object = Mock()  # Non-Client object
+        assert monitor.is_ignored_disabled_client(test_object) is expected_result
+
+    # More comprehensive overall tests using a real realm export
+    def test_find_sensitive_role(self, example_db):
+        # Test everything with the default settings
+        # fmt: off
+        # I'd like to keep this readable
+        config = {
+            "monitors": {
+                "ServiceAccountWithSensitiveRole": [
+                    {
+                        "allowed": [],
+                        "role": "sensitive-role",
+                        "role-client": "realm"
+                    }
+                ]
+            }
+        }
+        # fmt: on
+        monitor = ServiceAccountWithSensitiveRole(example_db, config)
+        results = [result for result in monitor.audit()]
+        assert len(results) == 5
+        for result in results:
+            assert result.to_dict()["entity"] in [
+                "service-account-client-with-service-account-with-sensitive-role",
+                "service-account-client-with-service-account-with-sensitive-composite-role",
+                "service-account-client-with-service-account-with-recursive-sensitive-role",
+                "service-account-service-account-client-with-service-account-in-sensitive-subgroup",
+                "service-account-client-with-service-account-in-recursive-sensitive-group",
+            ]
+
+    def test_ignore_specific_entity(self, example_db):
+        # fmt: off
+        config = {
+            "monitors": {
+                "ServiceAccountWithSensitiveRole": [
+                    {
+                        "allowed": ["service-account-client-with-service-account-with-sensitive-role"],
+                        "role": "sensitive-role",
+                        "role-client": "realm"
+                    }
+                ]
+            }
+        }
+        # fmt: on
+        monitor = ServiceAccountWithSensitiveRole(example_db, config)
+        results = [result for result in monitor.audit()]
+        assert len(results) == 4
+        for result in results:
+            assert result.to_dict()["entity"] in [
+                "service-account-client-with-service-account-with-sensitive-composite-role",
+                "service-account-client-with-service-account-with-recursive-sensitive-role",
+                "service-account-service-account-client-with-service-account-in-sensitive-subgroup",
+                "service-account-client-with-service-account-in-recursive-sensitive-group",
+            ]
+    
+    def test_ignore_by_regex(self, example_db):
+        # fmt: off
+        config = {
+            "monitors": {
+                "ServiceAccountWithSensitiveRole": [
+                    {
+                        "allowed": ["service-account-client-with-service-account-with-.*"],
+                        "role": "sensitive-role",
+                        "role-client": "realm"
+                    }
+                ]
+            }
+        }
+        # fmt: on
+        monitor = ServiceAccountWithSensitiveRole(example_db, config)
+        results = [result for result in monitor.audit()]
+        assert len(results) == 2
+        for result in results:
+            assert result.to_dict()["entity"] in [
+                "service-account-service-account-client-with-service-account-in-sensitive-subgroup",
+                "service-account-client-with-service-account-in-recursive-sensitive-group",
+            ]
+
+    def test_match_role_by_regex(self, example_db):
+        # fmt: off
+        config = {
+            "monitors": {
+                "ServiceAccountWithSensitiveRole": [
+                    {
+                        "allowed": [],
+                        "role": ".*-role$",
+                        "role-client": "realm"
+                    }
+                ]
+            }
+        }
+        # fmt: on
+        monitor = ServiceAccountWithSensitiveRole(example_db, config)
+        results = [result for result in monitor.audit()]
+        assert len(results) == 13
+
+    @pytest.mark.parametrize(
+            "ignore_disabled_clients, expected_number", [
+                (True, 4),  # When ignoring disabled clients, we expect to find 4
+                (False, 5), # When explicitly not ignoring them, we expect 5
+                (None, 5)   # When the value is not set, do not ignore by default
+            ]
+    )
+    def test_ignore_disabled_client(self, example_db, ignore_disabled_clients, expected_number):
+        # Ignore a specific entity
+        # fmt: off
+        config = {
+            "ignore_disabled_clients": ignore_disabled_clients,
+            "monitors": {
+                "ServiceAccountWithSensitiveRole": [
+                    {
+                        "allowed": [],
+                        "role": "sensitive-role",
+                        "role-client": "realm"
+                    }
+                ]
+            }
+        }
+        # fmt: on
+        example_db.get_client("client-with-service-account-with-sensitive-role")._d["enabled"] = False
+        monitor = ServiceAccountWithSensitiveRole(example_db, config)
+        results = [result for result in monitor.audit()]
+        assert len(results) == expected_number
