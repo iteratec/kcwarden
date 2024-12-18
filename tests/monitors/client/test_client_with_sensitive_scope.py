3acc2e4edf76350ac15b4f17c2e51a10dd7c1e63
Max Maass <max.maass@iteratec.com>
2024-12-18 14:13:55 +0100

    Add tests for ClientWithSensitiveScope monitor

---
+++ tests/monitors/client/test_client_with_sensitive_scope.py
@@ -0,0 +1,149 @@
+import pytest
+from unittest.mock import Mock
+
+from kcwarden.monitors.client.client_with_sensitive_scope import ClientWithSensitiveScope
+from kcwarden.custom_types import config_keys
+from kcwarden.custom_types.keycloak_object import ClientScope
+
+
+class TestClientWithSensitiveScope:
+    @pytest.fixture
+    def monitor(self, mock_database, default_config):
+        monitor_instance = ClientWithSensitiveScope(mock_database, default_config)
+        return monitor_instance
+
+    @pytest.fixture
+    def scope(self):
+        scope = Mock(spec=ClientScope)
+        scope.get_name.return_value = "sensitive-scope"
+        return scope
+
+    def test_audit_default_scope(self, monitor, scope, mock_client):
+        mock_client.get_default_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": scope.get_name(),
+                        "allowed": []
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        results = list(monitor.audit())
+        assert len(results) == 1
+        assert results[0].to_dict()["entity"] == mock_client.get_name()
+
+    def test_audit_optional_scope(self, monitor, scope, mock_client):
+        mock_client.get_optional_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": scope.get_name(),
+                        "allowed": []
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        results = list(monitor.audit())
+        assert len(results) == 1
+        assert results[0].to_dict()["entity"] == mock_client.get_name()
+
+    def test_audit_regex_match(self, monitor, scope, mock_client):
+        mock_client.get_optional_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": "sensitive-.*",
+                        "allowed": []
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        results = list(monitor.audit())
+        assert len(results) == 1
+        assert results[0].to_dict()["entity"] == mock_client.get_name()
+
+    def test_audit_allowlist(self, monitor, scope, mock_client):
+        mock_client.get_optional_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": "sensitive-.*",
+                        "allowed": [mock_client.get_name()]
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        results = list(monitor.audit())
+        assert len(results) == 0
+
+    def test_audit_allowlist_regex(self, monitor, scope, mock_client):
+        mock_client.get_optional_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": "sensitive-.*",
+                        "allowed": ["mock-.*"]
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        results = list(monitor.audit())
+        assert len(results) == 0
+
+    @pytest.mark.parametrize(
+        "ignore_disabled_clients, client_enabled, expected_result",
+        [
+            (True, False, 0),  # Disabled client, config to ignore
+            (True, True, 1),  # Enabled client, should never be ignored
+            (False, False, 1),  # Disabled client, config not to ignore
+        ],
+    )
+    def test_ignore_disabled_client_when_asked(
+        self, monitor, mock_client, ignore_disabled_clients, client_enabled, expected_result, scope
+    ):
+        # fmt: off
+        monitor._CONFIG = {
+            config_keys.IGNORE_DISABLED_CLIENTS: ignore_disabled_clients,
+            config_keys.MONITOR_CONFIG: {
+                "ClientWithSensitiveScope": [
+                    {
+                        "scope": "sensitive-.*",
+                        "allowed": []
+                    }
+                ]
+            },
+            config_keys.AUDITOR_CONFIG: {}
+        }
+        # fmt: on
+        mock_client.is_enabled.return_value = client_enabled
+        mock_client.get_optional_client_scopes.return_value = [scope.get_name()]
+        monitor._DB.get_all_clients.return_value = [mock_client]
+
+        results = list(monitor.audit())
+        assert len(results) == expected_result
