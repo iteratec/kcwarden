42504dc88bd6bc48b2e22fce2b71eb58267463bf
Nils Bergmann <n.bergmann@smf.de>
2025-12-11 12:25:32 +0100

    Add auditor for missing password policies

---
+++ tests/auditors/realm/test_password_policy_missing.py
@@ -0,0 +1,23 @@
+import pytest
+
+from kcwarden.auditors.realm.password_policy_missing import PasswordPolicyMissing
+from kcwarden.custom_types.keycloak_object import Realm
+
+
+class TestPasswordPolicyMissing:
+    @pytest.fixture
+    def auditor(self, mock_database, default_config):
+        return PasswordPolicyMissing(mock_database, default_config)
+
+    def test_should_consider_realm(self, auditor, mock_realm: Realm):
+        assert auditor.should_consider_realm(mock_realm) is True  # Always consider unless specifically ignored
+
+    def test_password_policy_empty(self, auditor, mock_realm: Realm):
+            # Test with no password policy set
+            mock_realm._d = {}
+            assert auditor.realm_has_no_password_policy(mock_realm)
+
+    def test_password_policy_set(self, auditor, mock_realm: Realm):
+            # Test with some password policy set
+            mock_realm._d = {"passwordPolicy": "length(15)"}
+            assert not auditor.realm_has_no_password_policy(mock_realm)
