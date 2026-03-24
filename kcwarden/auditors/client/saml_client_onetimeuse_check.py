4b2b755bbf319fc522253469ecf08bb772f323eb
Max Maass <max.maass@iteratec.com>
2026-03-24 11:57:53 +0100

    Refactor SAML client auditors to use correct base class

--- kcwarden/auditors/client/saml_client_onetimeuse_check.py
+++ kcwarden/auditors/client/saml_client_onetimeuse_check.py
@@ -1,18 +1,17 @@
-from kcwarden.api import Auditor
+from kcwarden.api.auditor import ClientAuditor
+from kcwarden.custom_types.keycloak_object import Client
 from kcwarden.custom_types.result import Severity
 
 
-class SamlClientOneTimeUseCheck(Auditor):
+class SamlClientOneTimeUseCheck(ClientAuditor):
     DEFAULT_SEVERITY = Severity.Medium
     SHORT_DESCRIPTION = "SAML OneTimeUse condition not enabled"
     LONG_DESCRIPTION = "Keycloak is not configured to add the <OneTimeUse> condition to SAML Assertions. This increases the risk of Replay Attacks if the Service Provider does not strictly track Assertion IDs."
     REFERENCE = ""
 
-    def should_consider_client(self, client) -> bool:
-        return self.is_not_ignored(client) and client.is_saml_client()
+    def should_consider_client(self, client: Client) -> bool:
+        return super().should_consider_client(client) and client.is_saml_client()
 
-    def audit(self):
-        for client in self._DB.get_all_clients():
-            if self.should_consider_client(client):
-                if not client.is_saml_onetimeuse_condition_enabled():
-                    yield self.generate_finding(client)
+    def audit_client(self, client: Client):
+        if not client.is_saml_onetimeuse_condition_enabled():
+            yield self.generate_finding(client)
--- kcwarden/auditors/client/saml_client_wildcard_redirect_uris.py
+++ kcwarden/auditors/client/saml_client_wildcard_redirect_uris.py
@@ -1,25 +1,24 @@
-from kcwarden.api import Auditor
+from kcwarden.api.auditor import ClientAuditor
+from kcwarden.custom_types.keycloak_object import Client
 from kcwarden.custom_types.result import Severity
 
 
-class SamlClientWildcardRedirectUriCheck(Auditor):
+class SamlClientWildcardRedirectUriCheck(ClientAuditor):
     DEFAULT_SEVERITY = Severity.Medium
     SHORT_DESCRIPTION = "Client allows wildcard redirect URIs"
     LONG_DESCRIPTION = "The client configuration contains a wildcard (*) at the end of a Redirect URI. This allows open redirects to subdirectories, potentially leading to token theft."
     REFERENCE = ""
 
-    def should_consider_client(self, client) -> bool:
-        return self.is_not_ignored(client) and client.is_saml_client()
+    def should_consider_client(self, client: Client) -> bool:
+        return super().should_consider_client(client) and client.is_saml_client()
 
-    def get_vulnerable_uris(self, client) -> list[str]:
+    def get_vulnerable_uris(self, client: Client) -> list[str]:
         uris = client.get_resolved_redirect_uris()
         if not uris:
             return []
         return [uri for uri in uris if uri and uri.strip().endswith("*")]
 
-    def audit(self):
-        for client in self._DB.get_all_clients():
-            if self.should_consider_client(client):
-                bad_uris = self.get_vulnerable_uris(client)
-                if bad_uris:
-                    yield self.generate_finding(client, additional_details={"vulnerable_uris": bad_uris})
+    def audit_client(self, client: Client):
+        bad_uris = self.get_vulnerable_uris(client)
+        if bad_uris:
+            yield self.generate_finding(client, additional_details={"vulnerable_uris": bad_uris})
